76f37d31030a0d9adc7dac3df582b58807839af5
Rain0Rice <rainunofficial8008@gmail.com>
2024-10-17 07:55:34 -0700

    Test D-pad code
    Adding power

--- Jave_Code_1.java
+++ Jave_Code_1.java
@@ -15,10 +15,12 @@ public class Jave_Code extends LinearOpMode{
 	//Call servos/motors variables for the future
 	private DcMotor _00_motor;
 	private DcMotor _01_motor;
+	private DcMotor shoulder_0;
+	private DcMotor shoulder_1;
 	private CRServo claew;
 	float LeftPower = 0;
 	float RightPower = 0;
-
+	String micro = "";
 	//Use the webcam on the robot
 	//private static final boolean USE_WEBCAM = true;
 	//Variables for apriltag & visionportal
@@ -31,8 +33,8 @@ public class Jave_Code extends LinearOpMode{
 		//Map our hardware on the robot to the variables previously created
 		 _00_motor = hardwareMap.get(DcMotor.class, "00_motor");
 		 _01_motor = hardwareMap.get(DcMotor.class, "01_motor");
-		 _0_shoulder = hardwareMap.get(DcMotor.class, "0_shoulder");
-		 _1_shoulder = hardwareMap.get(DcMotor.class, "1_shoulder");
+		 shoulder_0 = hardwareMap.get(DcMotor.class, "shoulder_0");
+		 shoulder_1 = hardwareMap.get(DcMotor.class, "shoulder_1");
 		 claew = hardwareMap.get(CRServo.class, "claew");
 
 		//April Tags(ERRORS, FIX)
@@ -44,7 +46,7 @@ public class Jave_Code extends LinearOpMode{
 		 waitForStart();
 		 //Set any motor flips needed
 		_01_motor.setDirection(DcMotor.Direction.REVERSE);
-		if (opModeIsActive()) {
+		if (opModeIsActive()); {
 			while (opModeIsActive()) {
 				float LeftPower = gamepad1.left_stick_y;
 				float RightPower = gamepad1.right_stick_y;
@@ -52,19 +54,34 @@ public class Jave_Code extends LinearOpMode{
 				//Movement for gamepad1(Trenton)
 				_00_motor.setPower(gamepad1.right_stick_y);
 				_01_motor.setPower(gamepad1.left_stick_y);
+				micro = "none";
+				if (gamepad1.right_stick_y == 0 && gamepad1.left_stick_y == 0);
+					if (gamepad2.dpad_left == true){ //microadjust the robot left
+						_00_motor.setPower(0.5);
+						_01_motor.setPower(-0.5);
+						micro = "L";
+						_00_motor.setPower(0);
+						_01_motor.setPower(0);
+					}
+					if (gamepad2.dpad_right == true){ //microadjust the robot right
+						_00_motor.setPower(-0.5);
+						_01_motor.setPower(0.5);
+						micro = "R";
+						_00_motor.setPower(0);
+						_01_motor.setPower(0);
+					}
 
 				//Arm control for gamepad2(Kelie)
-				_0_shoulder.setPower(gamepad2.left_stick_y)
-				_1_shoulder.setPower(gamepad2.left_stick_y)
-				if (gamepad2.d_pad_left == true){
-					_00_motor.setPower(0.25)
-					_01_motor.setPower(-0.25)
-					wait(100)
-					_00_motor.setPower(0)
-					_01_motor.setPower(0)
+				shoulder_0.setPower(gamepad2.left_stick_y);
+				shoulder_1.setPower(-(gamepad2.left_stick_y));
+				if (gamepad2.b == true) {
+					shoulder_0.setPower(-0.25);
+					shoulder_1.setPower(0.25);
 				}
+
+
 				if (gamepad2.a == true) {
-					claew.setPower(-0.25);
+					claew.setPower(-0.15);
 					telemetry.addData("Claw", "Open");
 				}
 				else {
@@ -75,7 +92,7 @@ public class Jave_Code extends LinearOpMode{
 				//Telementry calls
 				telemetry.addData("Left Motor", LeftPower); //The speed the robot is trying to move on the Left Motor
 				telemetry.addData("Right Motor", RightPower); //The speed the robot is trying to move on the Right Motor
-
+				telemetry.addData("micro", micro);
 				telemetry.update();
 		  }
 		}
